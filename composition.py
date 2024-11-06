d85f50a8527488ef7cdc2c4ccaeb4b09294f4c2e
Archiedog5 <jonathan.hatch@ucas-edu.net>
2024-11-06 11:50:08 -0700

    did some things

--- composition.py
+++ composition.py
@@ -9,13 +9,23 @@ class Car:
 
     def __str__(self):
         return f"{self.year} {self.make} {self.model}"
+
+    def __repr__(self):
+        return f"Car({self.make}, {self.model}, {self.year}, {self.engine})"
+    # __repr__ is for other programmers, and for debugging perpouces. Reper should tell them what class and all attributes
 class Engine:
     def __init__(self, configuration, displacement, horspower, torque):
         self.configuration=configuration
         self.displacement=displacement
         self.horsepower=horspower
         self.torque=torque
+
+    def __str__(self):
+        return f"The engine is a {self.configuration} with {self.displacement}, and has {self.horsepower} horsepower and {self.torque}"
     
+    def __repr__(self):
+        return f"Engine({self.configuration}, {self.displacement}, {self.horsepower}, {self.torque})"
+
     def ignite(self):
         print("Vroom, vroom!")
 
@@ -25,3 +35,5 @@ myCar= Car("Mazda", "Mazda3", 2013, myEngine)
 #To call a coposit class you must access where it is saveed whithin the class
 print(myCar)
 myCar.engine.ignite()
+print(repr(myCar))
+print(repr(myEngine))
