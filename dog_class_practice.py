98699063fbbfb942ff799626234d82bd73624c8e
Archiedog5 <jonathan.hatch@ucas-edu.net>
2024-11-07 05:20:57 +0000

    made dog class

---
+++ dog_class_practice.py
@@ -0,0 +1,10 @@
+class Dog:
+    def __init__(self, name, breed):
+        self.name=name
+        self.breed=breed
+dogs=[]
+dogs.append(Dog("Marceline","German Shepherd"))
+dogs.append(Dog("Cajun","Belgian Malinois"))
+dogs.append(Dog("Daisy","Border Collie"))
+dogs.append(Dog("Rocky","Golden Retriever"))
+dogs.append(Dog("Bella","Irish Setter"))
